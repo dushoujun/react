fa2422fdcbc2354c88ba33c22a159eb64c42c1ae
dushoujun <dushoujun@ZB-C45RN62.360buyAD.local>
2017-03-21 21:24:08 +0800

    test

--- hello/src/App.test.js
+++ hello/src/App.test.js
@@ -4,6 +4,5 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  div.className = 'hhhh'
   ReactDOM.render(<App />, div);
 });
--- hello/src/index.js
+++ hello/src/index.js
@@ -2,8 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './App';
 import './index.css';
+const food = [
+	'breakfast: porridge',
+	'lunch: vegetables',
+	'dinner: noodles'
+]
+
+function eat() {
+	let i = parseInt(Math.random() * 3);
+	let temp = <li>{food[i]}</li>;
+	return temp;
+}
+const element = (
+	<div>
+		<h1>What do we eat tomorrow?</h1>
+		<ul>
+			{eat()}
+		</ul>
+	</div>
+)
 
 ReactDOM.render(
-  <h1>Hello React</h1>,
+  element,
   document.getElementById('root')
 );
